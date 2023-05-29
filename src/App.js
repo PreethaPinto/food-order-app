ac6dc6a3e16cf47171ca999d39cd92f91d84f74a
PreethaPinto <preethacp@gmail.com>
2023-05-29 13:04:04 +1000

    Add cart, form, modal components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
