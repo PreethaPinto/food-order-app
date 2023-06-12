f496a539d3fbe4aba67a51d0c3ac7809f799729b
PreethaPinto <preethacp@gmail.com>
2023-06-13 00:17:25 +1000

    Add form validation and submitting and sending cart data

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -9,7 +9,7 @@ const MealItem = (props) => {
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addItemToCartHandler = (amount) => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
@@ -21,12 +21,12 @@ const MealItem = (props) => {
   return (
     <li className={classes.meal}>
       <div>
-        <h3> {props.name} </h3>
-        <div className={classes.description}> {props.description}</div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} onAddToCart={addItemToCartHandler} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
